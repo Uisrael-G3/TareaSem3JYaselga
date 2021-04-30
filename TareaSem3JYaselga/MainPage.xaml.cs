873fa42a603c6e1fca10b62e1f2de158b73f60a5
Edu-SSD <Edu-SSD@DESKTOP-01M5JPA>
2021-04-30 06:04:57 -0500

    Agregar archivos de proyecto.

---
+++ TareaSem3JYaselga/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TareaSem3JYaselga
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrirVentana_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+
+                string usuario = txtUsuario.Text;
+                string clave = txtClave.Text;
+
+                if (usuario == "estudiante2021" && clave == "uisrael2021")
+                {
+                    await Navigation.PushAsync(new Ventana2(usuario, clave));
+                }
+                else
+                    await DisplayAlert("Alerta", "Usuario o Contraseña son incorrectos", "OK");
+
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Alerta", ex.Message, "OK");
+            }
+
+        }
+    }
+}
---
+++ TareaSem3JYaselga/Ventana2.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TareaSem3JYaselga
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Ventana2 : ContentPage
+    {
+        public Ventana2(string usuario, string clave)
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Clicked(object sender, EventArgs e)
+        {
+			try
+			{
+				//Acciones del programa
+				double dato1 = Convert.ToDouble(txtDato1.Text);
+				double dato2 = Convert.ToDouble(txtDato2.Text);
+
+				double dato3 = Convert.ToDouble(txtDato3.Text);
+				double dato4 = Convert.ToDouble(txtDato4.Text);
+
+				double suma = (dato1 / 10) * 3;
+				double suma2 = (dato2 / 10) * 2;
+				double notap1 = suma + suma2;
+
+				double suma3 = (dato3 / 10) * 3;
+				double suma4 = (dato4 / 10) * 2;
+				double notap2 = suma3 + suma4;
+
+				double nfinal = notap1 + notap2;
+
+				txtResultado1.Text = notap1.ToString();
+				txtResultado3.Text = notap2.ToString();
+				txtNfinal.Text = nfinal.ToString();
+
+				if (nfinal >= 7)
+				{
+
+					txtEstado.Text = "Aprobado";
+				}
+				if (nfinal >= 5 && nfinal <= 6.9)
+				{
+					txtEstado.Text = "Complementario";
+				}
+
+				if (nfinal < 7)
+				{
+
+					txtEstado.Text = "Reprobado";
+				}
+
+			}
+			catch (Exception ex)
+			{
+				//Excepciones  - Errores del programa
+				DisplayAlert("Alerta", ex.Message, "OK");
+
+			}
+		}
+    }
+}
